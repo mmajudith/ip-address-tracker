58207813d31260ed921ae42126dae8c4352d726a
mma judith <mmajudith2000@gmail.com>
2020-10-05 05:33:48 -0700

    Updates

---
+++ precache-manifest.fc618b58aa4e84fe8c21a8c4d3a47caf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97acb5887f62876293a4264e89d2cc53",
+    "url": "/ip-address-tracker/index.html"
+  },
+  {
+    "revision": "738b2d2aec4b2c9711f1",
+    "url": "/ip-address-tracker/static/css/main.fc5f9dc8.chunk.css"
+  },
+  {
+    "revision": "af3e21d2a83eb3aa9d3b",
+    "url": "/ip-address-tracker/static/js/2.64ed9820.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/ip-address-tracker/static/js/2.64ed9820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738b2d2aec4b2c9711f1",
+    "url": "/ip-address-tracker/static/js/main.2cfc8f49.chunk.js"
+  },
+  {
+    "revision": "5104e4c6f07701f2a694",
+    "url": "/ip-address-tracker/static/js/runtime-main.b7b6add5.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/ip-address-tracker/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/ip-address-tracker/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-address-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
