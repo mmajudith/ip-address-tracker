2958a3cd9f12854a92496f2818229d338e761f97
mma judith <mmajudith2000@gmail.com>
2020-10-05 05:25:43 -0700

    IP tracker

---
+++ src/components/Attribution.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Attribution() {
+  return (
+    <div>
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/mmajudith">Onyekwelu Judith</a>.
+        </div>
+    </div>
+  )
+}
+
+export default Attribution
